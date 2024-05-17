80af300f2035d65f49adb6abfe5049da46e86d2c
Poppy <poppy.sari@rga.com>
2024-05-17 06:38:45 +0400

    fix[mailchimp]: submission failed due to empty response

--- src/components/templates/MailChimp.tsx
+++ src/components/templates/MailChimp.tsx
@@ -6,7 +6,7 @@ const MailChimp = () => {
     'https://rga.us9.list-manage.com/subscribe/post?u=edb8efdab33d9d8abfdfb4e68&amp;id=b254349d5c&amp;f_id=007317e0f0'
 
   const { loading, error, success, message, handleSubmit } = useMailChimpForm(url)
-  const fields = useFormFields({
+  const { fields, handleFieldChange } = useFormFields({
     EMAIL: 'sam.brunno@rga.com',
   })
   return (
@@ -17,13 +17,7 @@ const MailChimp = () => {
           handleSubmit(fields)
         }}
       >
-        <input
-          id="EMAIL"
-          autoFocus
-          type="email"
-          value={'sam.brunno@rga.com'}
-          // onChange={handleFieldChange}
-        />
+        <input id="EMAIL" autoFocus type="email" value={'sam.brunno@rga.com'} onChange={handleFieldChange} />
         <button>submit</button>
       </form>
       {loading && 'submitting'}
