bf9be9131be749e0c34e874585b31f204aa955b1
Poppy <poppy.sari@rga.com>
2024-05-27 08:45:25 +0400

    chore[SEO]: add schema

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,24 @@ class MyDocument extends Document {
             (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K58H34N5');`,
             }}
           />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: `{
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "url": "https://genonereport.com.au ",
+        "name": "GenOne Report",
+        "alternateName": " Digital Native Research Project",
+        "description": "Discover the digital behaviors of Digital Natives and Digital Immigrants in Australia. Our research report outlines key strategies to enhance digital engagement.",
+        "publisher": {
+            "@type": "Organization",
+            "name": "R/GA Australia",
+            "url": "https://rga.com/offices/australia"
+        }
+    }`,
+            }}
+          ></script>
 
           <ColorSchemeScript />
         </Head>
