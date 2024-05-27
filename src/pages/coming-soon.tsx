9af180978a47a5d7ab2abc01409dca9ac8b0ab4f
Poppy <poppy.sari@rga.com>
2024-05-27 17:19:06 +0400

    temp: check point 1 - desktop 2 box

--- src/pages/coming-soon.tsx
+++ src/pages/coming-soon.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import dynamic from 'next/dynamic'
 import { em } from '@mantine/core'
@@ -19,7 +19,8 @@ import ModalRegister from '@/organisms/modalRegister/ModalRegister'
 const Header = dynamic(() => import('@/organisms/header/Header'), { ssr: false })
 
 export default function ComingSoon() {
-  const { scrollYProgress } = useScroll()
+  const scrollRef = useRef(null)
+  const { scrollYProgress } = useScroll({ target: scrollRef, offset: ['start end', 'end start'] })
   const { height: viewportHeight, width: viewportWidth } = useViewportSize()
   const vhToPx = (vh: number) => (vh / 100) * viewportHeight
   const vwToPx = (vw: number) => (vw / 100) * viewportWidth
@@ -30,45 +31,49 @@ export default function ComingSoon() {
     threshold: 0.3,
   })
 
+  const section2Start = 0.35
+
   const isMobile = useMediaQuery(`(max-width: ${em(CONTAINER_SIZES.lg)})`)
 
-  const x = useTransform(scrollYProgress, [0.1, 0.8], [vwToPx(50) - 292, vwToPx(155)])
-  const y = useTransform(scrollYProgress, [0.1, 0.8], [vhToPx(100) - 600, vhToPx(230)])
+  const blueX = useTransform(scrollYProgress, [0.2, section2Start], [vwToPx(20), viewportWidth - 340])
+  const blueY = useTransform(scrollYProgress, [0.2, section2Start], [vhToPx(100) - 520, vhToPx(140)])
 
-  console.log('<====== scrollYProgress', scrollYProgress)
+  const pinkYellowX = useTransform(scrollYProgress, [0.1, section2Start], [vwToPx(50) - 90, viewportWidth - 180])
+  const pinkYellowY = useTransform(scrollYProgress, [0.1, section2Start], [vhToPx(60) - 170, vhToPx(120)])
+
+  const pinkYellowRotate = useTransform(scrollYProgress, [0.21, section2Start], [90, -90])
+  const pinkYellowW = useTransform(scrollYProgress, [0.3, section2Start], [201, 44])
+  const pinkYellowH = useTransform(scrollYProgress, [0.3, section2Start], [195, 267])
+  console.log('useProgress', scrollYProgress)
+
   return (
     <div
       className={cn(
         'px-0 mx-0',
         '!w-screen !max-w-screen min-h-screen !h-full',
-        'flex flex-col dn-scroll-container relative bg-black',
+        'flex flex-col dn-scroll-container overflow-hidden relative bg-black',
       )}
+      ref={scrollRef}
     >
       <Header mode={isIntersecting ? 'light' : 'dark'} handleRegister={open} />
+
       <motion.div
-        className="dn-blue-box absolute h-[317px] w-[223px] bg-ford-blue text-black mix-blend-plus-lighter lg:!h-[292px] lg:!w-[743px]"
-        style={{ x, y }}
+        className="dn-blue-box absolute h-[317px] w-[223px] bg-ford-blue mix-blend-plus-lighter lg:!h-[292px] lg:!w-[743px]"
+        style={{ x: blueX, y: blueY }}
         initial={{ opacity: 0.3 }}
         whileInView={{ opacity: 1 }}
         transition={{ delay: 0, ease: 'easeInOut' }}
       />
-      {/* <motion.div
-        className={cn(
-          'dn-blue-box bg-ford-blue',
-          'absolute inset-0',
-          ' w-[223px] lg:!w-[743px] h-[317px] lg:!h-[292px]',
-        )}
-        initial={{ opacity: 0.3 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ delay: 0, ease: 'easeInOut' }}
-        style={{
-          x: useTransform(scrollYProgress, [0.1, 0.4], [0, vwToPx(80)]),
-          y: useTransform(scrollYProgress, [0.1, 0.4], [0, vhToPx(20)]),
-        }}
-      /> */}
 
       <SectionOneTwo isMobile={isMobile} />
       <SectionTwo isMobile={isMobile} />
+      <motion.div
+        className="dn-box-6 absolute h-[195.102px] w-[201.276px] bg-gr-pink-yellow !mix-blend-plus-lighter"
+        style={{ x: pinkYellowX, y: pinkYellowY, rotate: pinkYellowRotate, width: pinkYellowW, height: pinkYellowH }}
+        initial={{ opacity: 0.3 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0, ease: 'easeInOut' }}
+      />
 
       <div ref={ref} className="flex h-full flex-col bg-white">
         <SectionThree />
