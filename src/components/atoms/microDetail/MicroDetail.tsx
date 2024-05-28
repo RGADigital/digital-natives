6f0500a24ed1a13b1ec38fcc70f49c9cc243af4d
Poppy <poppy.sari@rga.com>
2024-05-28 11:23:50 +0400

    fix[micro-detail]: leading

--- src/components/atoms/microDetail/MicroDetail.tsx
+++ src/components/atoms/microDetail/MicroDetail.tsx
@@ -13,16 +13,7 @@ export default function MicroDetail({ boxClassName, text, textClassName }: Reado
     <div className="dn-micro-detail z-20 flex items-center gap-[9px]">
       <div className={cn('!size-[11px] shrink-0', 'bg-accents-pink', boxClassName)}></div>
       {text && (
-        <Text
-          className={cn(
-            'text-[10px] lg:text-[14px]',
-            'leading-3 lg:leading-[16.8px] -tracking-[0.5px]',
-            'font-normal',
-            'uppercase',
-            textClassName,
-          )}
-          component="p"
-        >
+        <Text className={cn('text-m-eyebrow lg:text-eyebrow', 'uppercase', textClassName)} component="p">
           {text}
         </Text>
       )}
