d9db7c87b7221282773025f3ceded4bc303ba4eb
Poppy <poppy.sari@rga.com>
2024-06-19 11:42:27 +0400

    feat[header]: menu item

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,2 +1,3 @@
 export * from './button/Button'
+export { default as MicroDetail } from './microDetail/MicroDetail'
 export { default as MotionDiv } from './motionDiv'
---
+++ src/components/molecules/menu/menu.constant.ts
@@ -0,0 +1,57 @@
+const menuContent = [
+  {
+    microDetail: 'Chapter 01',
+    link: '/executive-summary',
+    title: 'Executive Summary',
+    isComingSoon: false,
+  },
+  {
+    microDetail: 'Chapter 02',
+    link: '/chapter-02',
+    title: 'Who is Gen One?',
+    isComingSoon: false,
+  },
+  {
+    microDetail: 'Chapter 03',
+    link: '/chapter-03',
+    title: 'Relationship to Technology',
+    isComingSoon: true,
+  },
+  {
+    microDetail: 'Chapter 04',
+    link: '/chapter-04',
+    title: 'Digital Diet & Behaviours',
+    isComingSoon: true,
+  },
+  {
+    microDetail: 'Chapter 05',
+    link: '/chapter-05',
+    title: 'Attention Economy',
+    isComingSoon: true,
+  },
+  {
+    microDetail: 'Chapter 06',
+    link: '/chapter-06',
+    title: 'Trust, Data & Security',
+    isComingSoon: true,
+  },
+  {
+    microDetail: 'Chapter 07',
+    link: '/chapter-07',
+    title: 'Government Services & Comms',
+    isComingSoon: true,
+  },
+  {
+    microDetail: 'Chapter 08',
+    link: '/chapter-08',
+    title: 'Reimaging the Digital World',
+    isComingSoon: true,
+  },
+]
+
+export const LANG = {
+  buttonMenu: 'Menu',
+  buttonMenuClose: 'Close',
+  comingSoon: 'Coming soon',
+  menuContent,
+}
