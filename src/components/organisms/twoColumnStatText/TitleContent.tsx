e2a3973a4ac6d0be3f044df2df806ef3a1446871
Poppy <poppy.sari@rga.com>
2024-06-24 15:29:43 +0400

    style[exec-summary]: [mobile] fix accordion part 1

--- src/components/organisms/twoColumnStatText/TitleContent.tsx
+++ src/components/organisms/twoColumnStatText/TitleContent.tsx
@@ -8,10 +8,10 @@ export default function TitleContent({
 }: Readonly<{ title: TwoColumnStatTextProps['title']; titleNumber: TwoColumnStatTextProps['titleNumber'] }>) {
   return (
     <>
-      <div className="col-span-12 lg:col-span-10">
+      <div className="order-2 col-span-12 lg:order-1 lg:col-span-10">
         <Title
           order={2}
-          className="font-m-h1 lg:font-h2 lg:!text-h2 order-2 text-m-h1 text-black lg:order-1 lg:border-b lg:border-t-0 lg:border-b-neutrals-cool-gray-0 lg:py-[25px] lg:pl-5"
+          className="font-m-h1 lg:font-h2 lg:!text-h2 text-m-h1 text-black  lg:border-b lg:border-t-0 lg:border-b-neutrals-cool-gray-0 lg:py-[25px] lg:pl-5"
         >
           {title}
         </Title>
