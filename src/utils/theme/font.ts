a46b35aca29818f39e01f239a6523b50d8ae67c7
Poppy <poppy.sari@rga.com>
2024-06-25 15:14:09 +0400

    style: update font change on figma

--- src/utils/theme/font.ts
+++ src/utils/theme/font.ts
@@ -1,3 +1,5 @@
+export const FONT_FAMILY = 'HelveticaNowDisplay'
+
 export const FONT_SIZES = {
   xs: '10px',
   sm: '12px',
@@ -5,23 +7,16 @@ export const FONT_SIZES = {
   md: '16px',
 }
 
-export const FONT_EYEBROW = {
-  fontSize: '14px',
-  fontWeight: 400,
-  lineHeight: '16.8px',
-  letterSpacing: '0.14px',
-}
-export const FONT_EYEBROW_MOBILE = {
-  fontSize: '10px',
-  fontWeight: 400,
-  lineHeight: '12px',
-  letterSpacing: '0.1px',
-}
-
 export const FONT_BODY = {
   fontSize: '16px',
   fontWeight: 400,
   lineHeight: '19.2px',
+}
+
+export const FONT_BODY_2 = {
+  fontSize: '12px',
+  fontWeight: 400,
+  lineHeight: '14.4px',
 }
 
 export const FONT_BODY_MOBILE = {
@@ -60,12 +55,66 @@ export const FONT_H2 = {
   letterSpacing: '-0.03em',
 }
 
-export const FONT_H3_MOBILE = {
-  fontSize: '26px',
-  fontWeight: '500',
-  lineHeight: '31.2px',
-  letterSpacing: '-0.78px',
+export const FONT_H2_MOBILE = {
+  fontSize: '35px',
+  fontWeight: 400,
+  lineHeight: '42px',
+  letterSpacing: '1%',
 }
+
+export const FONT_H3 = {
+  fontSize: '26px',
+  fontWeight: 400,
+  lineHeight: '31.2px',
+  letterSpacing: '-3%',
+}
+
+export const FONT_H3_MOBILE = {
+  fontSize: '24px',
+  fontWeight: '400',
+  lineHeight: '26.4px',
+  letterSpacing: '-3%',
+}
+
+export const FONT_SUBHEADING = {
+  fontSize: '24px',
+  fontWeight: '400',
+  lineHeight: '28.8px',
+}
+
+export const FONT_SUBHEADING_MOBILE = {
+  fontSize: '22px',
+  fontWeight: '400',
+  lineHeight: '26.4px',
+}
+
+export const FONT_EYEBROW = {
+  fontSize: '14px',
+  fontWeight: 400,
+  lineHeight: '16.8px',
+  letterSpacing: '0.14px',
+}
+export const FONT_EYEBROW_MOBILE = {
+  fontSize: '10px',
+  fontWeight: 400,
+  lineHeight: '12px',
+  letterSpacing: '0.1px',
+}
+
+export const FONT_QUOTE = {
+  fontSize: '22px',
+  fontWeight: '400',
+  lineHeight: '26.4px',
+}
+
+export const FONT_QUOTE_MOBILE = {
+  fontSize: '20px',
+  fontWeight: '400',
+  lineHeight: '24px',
+}
+
+// TODO: check if those fonts still used
+// only on coming soon page
 export const FONT_FORM = {
   fontSize: '14px',
   fontWeight: 400,
@@ -82,24 +131,4 @@ export const FONT_DISCLAIMER_MOBILE = {
   fontSize: '10px',
   fontWeight: '400',
   lineHeight: '12px',
-}
-
-export const FONT_FAMILY = 'HelveticaNowDisplay'
-
-export const FONT_QUOTE = {
-  fontSize: '22px',
-  fontWeight: '400',
-  lineHeight: '26.4px',
-}
-
-export const FONT_QUOTE_MOBILE = {
-  fontSize: '20px',
-  fontWeight: '400',
-  lineHeight: '24px',
-}
-
-export const FONT_SUBHEADING = {
-  fontSize: '24px',
-  fontWeight: '400',
-  lineHeight: '28.8px',
 }
